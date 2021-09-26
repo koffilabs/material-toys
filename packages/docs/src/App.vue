3057d05d61d9afe21fe3c585b037abc6d1c2f69b
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-09-26 14:34:55 +0200

    feat: dynamic themes using computed

--- packages/docs/src/App.vue
+++ packages/docs/src/App.vue
@@ -6,15 +6,20 @@
 import {css, cx} from "@emotion/css";
 import { theme } from "@material-yue/vue";
 import { Button } from "@material-yue/vue";
-import {provide} from "vue";
+import {provide, reactive} from "vue";
 
 
 export default {
   name: "material-yue-docs",
   components: {Button},
   setup() {
-    provide("theme", theme);
-
+    console.log(theme)
+    const reactiveTheme = reactive({...theme});
+    provide("theme", reactiveTheme);
+    setTimeout(() => {
+      console.log("changing the primary color to red");
+      reactiveTheme.colors.primary = "red";
+    }, 2000);
     const styled = css`
    border: 1px solid red;
  `;
--- packages/material-yue/src/components/Button.vue
+++ packages/material-yue/src/components/Button.vue
@@ -2,16 +2,16 @@
   <div :class="btn">Button here</div>
 </template>
 <script>
-import {inject} from "vue";
+import {computed, inject, reactive, ref} from "vue";
 import {css, cx} from "@emotion/css";
 
 export default {
   name: "Button",
   setup(){
     const theme = inject("theme");
-    const btn = css`
+    let btn = computed(() => css`
       background:${theme.colors.primary};
-    `;
+    `);
     return { btn };
   }
 }
