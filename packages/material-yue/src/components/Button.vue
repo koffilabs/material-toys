b023de11c964bb59e1e913dd20874662862dce39
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-09-26 22:54:11 +0200

    feat: styles from the default theme, poc

--- packages/material-yue/src/components/Button.vue
+++ packages/material-yue/src/components/Button.vue
@@ -8,9 +8,11 @@ import {css, cx} from "@emotion/css";
 export default {
   name: "Button",
   setup(){
+    // TODO: refactor the Object.keys... thing in a reusable function
     const theme = inject("theme");
     let btn = computed(() => css`
       background:${theme.colors.primary};
+      ${Object.keys(theme.components.Button).map(key => `${key}:${theme.components.Button[key]};`)}
     `);
     return { btn };
   }
