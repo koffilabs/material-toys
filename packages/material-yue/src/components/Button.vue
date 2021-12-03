8cc5d0f4c7f5fe487f134bfa33cdadf4c2134f91
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-12-03 17:19:18 +0100

    feat: ripple, vue implementation start

--- packages/material-yue/src/components/Button.vue
+++ packages/material-yue/src/components/Button.vue
@@ -1,17 +1,22 @@
 <template>
-  <button :class="btn">
+  <Ripple>
+  <button v-bind="$attrs" :class="btn">
     <div class="state">
     </div>
     <slot name="icon"></slot>
     <div><slot></slot></div></button>
+  </Ripple>
 </template>
 <script lang="ts">
 import { computed, inject, Ref, ref } from "vue";
 import { css } from "@emotion/css";
 import { setDynamic, m3 } from "@material-yue/common";
+import Ripple from "./Ripple";
 
 export default {
+  inheritAttrs: false,
   name: "Button",
+  components: {Ripple},
   setup(){
     const tokens: any = inject("tokens");
     const variant: any = inject("variant");
---
+++ packages/material-yue/src/components/Ripple.vue
@@ -0,0 +1,19 @@
+<template>
+  <div @click="onClick">
+  <slot></slot>
+  </div>
+</template>
+<script lang="ts">
+export default {
+  name: "Ripple",
+  setup(props, context){
+    console.log("slots are", context.slots.default);
+    const onClick = () => {
+      console.log("clicked!")
+    }
+    return {
+      onClick
+    }
+  }
+}
+</script>
