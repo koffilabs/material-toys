48c1e2f249c453924f14029b12f938b8bd3fc79c
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-12-11 18:20:12 +0100

    fix: props

--- packages/react/src/components/Button.tsx
+++ packages/react/src/components/Button.tsx
@@ -1,29 +1,37 @@
-import React, {
-  useContext,
-} from "react";
+import React, { useContext } from "react";
 import { css } from "@emotion/css";
 import { applyReactiveStyle, m3 } from "@material-yue/common";
 import { useTheme } from "../hooks/useTheme";
 import { Ripple } from "./Ripple";
 interface ButtonProps {
   icon: any;
+  children?: JSX.Element;
   className: string;
   disabled: boolean;
   onClick?: Function;
 }
-export const Button = ({ icon, disabled, children, className }: ButtonProps) : JSX.Element => {
+export const Button = ({
+  icon,
+  disabled,
+  children,
+  className,
+}: ButtonProps): JSX.Element => {
   const { ThemeContext, VariantContext } = useTheme();
   const tokens = useContext(ThemeContext);
   const variant = useContext(VariantContext);
 
   const theme = m3(tokens, { variant });
 
-  const btn = css(applyReactiveStyle({ target: m3(tokens, { variant }).components.Button, theme })
+  const btn = css(
+    applyReactiveStyle({
+      target: m3(tokens, { variant }).components.Button,
+      theme,
+    })
   );
   return (
     <Ripple>
       <button className={`${btn} ${className}`}>
-        <div className="state"/>
+        <div className="state" />
         {icon}
         <div>{children}</div>
       </button>
