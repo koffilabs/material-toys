9b6b9a5a3d769c9d96459f72f35a456ab8c77532
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-12-11 17:33:17 +0100

    fix: crossOrigin attribute value

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -1,143 +1,172 @@
-import Head from 'next/head'
-import {EditIcon, AddIcon} from "@material-yue/icons-react";
-import {Button, Card, ExtendedFAB, FAB, Grid, useTheme} from "@material-yue/react";
+import Head from "next/head";
+import { EditIcon, AddIcon } from "@material-yue/icons-react";
+import {
+  Button,
+  Card,
+  ExtendedFAB,
+  FAB,
+  Grid,
+  useTheme,
+} from "@material-yue/react";
 //
-import {material_tokens} from "@material-yue/common";
-import {material_tokens_polyfill} from "@material-yue/common";
-import {useEffect, useState} from "react";
+import { material_tokens } from "@material-yue/common";
+import { material_tokens_polyfill } from "@material-yue/common";
+import { useEffect, useState } from "react";
 
-const tokens = {...material_tokens_polyfill, ...material_tokens};
+const tokens = { ...material_tokens_polyfill, ...material_tokens };
 // tokens ok
 export default function Home() {
-    const {ThemeContext, VariantContext} = useTheme();
-    const [reactiveTokens, setReactiveTokens] = useState(tokens);
-    // useEffect(() => {
-    //
-    //     setTimeout(() => {
-    //         tokens.MdSysColorPrimary = "red";
-    //         setReactiveTokens({...tokens});
-    //         // variant.value = "";
-    //         console.log("done")
-    //     }, 2000);
-    // }, [])
-    return (
-        <ThemeContext.Provider value={reactiveTokens}>
-            <VariantContext.Provider value={""}>
-                <div className="container">
-                    <Head>
-                        <title>Create Next App</title>
-                        <link rel="icon" href="/favicon.ico"/>
-                        <link rel="preconnect" href="https://fonts.googleapis.com"/>
-                        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-                        <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet"/>
-                    </Head>
+  const { ThemeContext, VariantContext } = useTheme();
+  const [reactiveTokens, setReactiveTokens] = useState(tokens);
+  // useEffect(() => {
+  //
+  //     setTimeout(() => {
+  //         tokens.MdSysColorPrimary = "red";
+  //         setReactiveTokens({...tokens});
+  //         // variant.value = "";
+  //         console.log("done")
+  //     }, 2000);
+  // }, [])
+  return (
+    <ThemeContext.Provider value={reactiveTokens}>
+      <VariantContext.Provider value={""}>
+        <div className="container">
+          <Head>
+            <title>Create Next App</title>
+            <link rel="icon" href="/favicon.ico" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link
+              rel="preconnect"
+              href="https://fonts.gstatic.com"
+              crossOrigin="true"
+            />
+            <link
+              href="https://fonts.googleapis.com/css2?family=Roboto&display=swap"
+              rel="stylesheet"
+            />
+          </Head>
 
-                    <main>
-                        <div>
-                            <Button icon={<AddIcon size="18px"/>} className="elevated">Elevated Button</Button>
-                            <Button icon={<AddIcon size="18px"/>} className="filled">Filled Button</Button>
-                            <Button icon={<AddIcon size="18px"/>} className="filled-tonal">Filled Tonal Button</Button>
-                            <Button icon={<AddIcon size="18px"/>} className="outlined">Outlined Button</Button>
-                            <Button icon={<AddIcon size="18px"/>} className="text">Text Button</Button>
-                        </div>
-                        <div>
-                            <FAB className="surface"><EditIcon/></FAB>
-                            <FAB><EditIcon/></FAB>
-                            <FAB className="level1"><EditIcon/></FAB>
-                            <FAB className="large"><EditIcon size="36px"/></FAB>
-                            <FAB className="level1 large"><EditIcon size="36px"/></FAB>
-                            <FAB className="level1 small"><EditIcon/></FAB>
-                            <FAB className="level1 small" disabled><EditIcon/></FAB>
-                        </div>
-                        <div>
-                            <h1>Extended FAB</h1>
-                            <ExtendedFAB>
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="surface">
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="secondary">
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="tertiary">
-                                Compose
-                            </ExtendedFAB>
-                        </div>
-                        <div>
-                            <h1>Extended FAB with icons</h1>
-                            <ExtendedFAB>
-                                <EditIcon/>
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="surface">
-                                <EditIcon/>
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="secondary">
-                                <EditIcon/>
-                                Compose
-                            </ExtendedFAB>
-                            <ExtendedFAB className="tertiary">
-                                <EditIcon/>
-                                Compose
-                            </ExtendedFAB>
-                        </div>
-                        <div>
-                            <Card className="elevated">
-                                <div className="someContent">
-                                    <h3>Title - Elevated</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-                            <Card className="filled">
-                                <div className="someContent">
-                                    <h3>Title - Elevated</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-                            <Card className="outlined">
-                                <div className="someContent">
-                                    <h3>Title - Elevated</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-
-                        </div>
-                        <Grid>
-                            <Card className="elevated col-lg-3 col-12 col-sm-6">
-                                <div className="someContent">
-                                    <h3>Title - Elevated</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-                            <Card className="filled col-lg-3 col-12 col-sm-6">
-                                <div className="someContent">
-                                    <h3>Title - Filled</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-                            <Card className="outlined col-lg-3 col-12">
-                                <div className="someContent">
-                                    <h3>Title - Outlined</h3>
-                                    <h5>Subhead</h5>
-                                    Use a card to display content and actions on a single topic.
-                                    Cards should be easy to scan for relevant information.
-                                </div>
-                            </Card>
-                        </Grid>
-                    </main>
-                    <style jsx>{`
+          <main>
+            <div>
+              <Button icon={<AddIcon size="18px" />} className="elevated">
+                Elevated Button
+              </Button>
+              <Button icon={<AddIcon size="18px" />} className="filled">
+                Filled Button
+              </Button>
+              <Button icon={<AddIcon size="18px" />} className="filled-tonal">
+                Filled Tonal Button
+              </Button>
+              <Button icon={<AddIcon size="18px" />} className="outlined">
+                Outlined Button
+              </Button>
+              <Button icon={<AddIcon size="18px" />} className="text">
+                Text Button
+              </Button>
+            </div>
+            <div>
+              <FAB className="surface">
+                <EditIcon />
+              </FAB>
+              <FAB>
+                <EditIcon />
+              </FAB>
+              <FAB className="level1">
+                <EditIcon />
+              </FAB>
+              <FAB className="large">
+                <EditIcon size="36px" />
+              </FAB>
+              <FAB className="level1 large">
+                <EditIcon size="36px" />
+              </FAB>
+              <FAB className="level1 small">
+                <EditIcon />
+              </FAB>
+              <FAB className="level1 small" disabled>
+                <EditIcon />
+              </FAB>
+            </div>
+            <div>
+              <h1>Extended FAB</h1>
+              <ExtendedFAB>Compose</ExtendedFAB>
+              <ExtendedFAB className="surface">Compose</ExtendedFAB>
+              <ExtendedFAB className="secondary">Compose</ExtendedFAB>
+              <ExtendedFAB className="tertiary">Compose</ExtendedFAB>
+            </div>
+            <div>
+              <h1>Extended FAB with icons</h1>
+              <ExtendedFAB>
+                <EditIcon />
+                Compose
+              </ExtendedFAB>
+              <ExtendedFAB className="surface">
+                <EditIcon />
+                Compose
+              </ExtendedFAB>
+              <ExtendedFAB className="secondary">
+                <EditIcon />
+                Compose
+              </ExtendedFAB>
+              <ExtendedFAB className="tertiary">
+                <EditIcon />
+                Compose
+              </ExtendedFAB>
+            </div>
+            <div>
+              <Card className="elevated">
+                <div className="someContent">
+                  <h3>Title - Elevated</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+              <Card className="filled">
+                <div className="someContent">
+                  <h3>Title - Elevated</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+              <Card className="outlined">
+                <div className="someContent">
+                  <h3>Title - Elevated</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+            </div>
+            <Grid>
+              <Card className="elevated col-lg-3 col-12 col-sm-6">
+                <div className="someContent">
+                  <h3>Title - Elevated</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+              <Card className="filled col-lg-3 col-12 col-sm-6">
+                <div className="someContent">
+                  <h3>Title - Filled</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+              <Card className="outlined col-lg-3 col-12">
+                <div className="someContent">
+                  <h3>Title - Outlined</h3>
+                  <h5>Subhead</h5>
+                  Use a card to display content and actions on a single topic.
+                  Cards should be easy to scan for relevant information.
+                </div>
+              </Card>
+            </Grid>
+          </main>
+          <style jsx>{`
                       .someContent {
                         font-family: "Roboto", sans-serif;
                         padding: 16px 0;
@@ -156,8 +185,8 @@ export default function Home() {
                       )
 
                     `}</style>
-                </div>
-            </VariantContext.Provider>
-        </ThemeContext.Provider>
-    )
+        </div>
+      </VariantContext.Provider>
+    </ThemeContext.Provider>
+  );
 }
