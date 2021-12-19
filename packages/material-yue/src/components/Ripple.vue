fa20071977cefe4f0da436a4c9b0bbe718af9657
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-12-19 15:55:59 +0100

    feat: using pointer events in vue

--- packages/material-yue/src/components/Ripple.vue
+++ packages/material-yue/src/components/Ripple.vue
@@ -10,17 +10,17 @@ export default {
   setup(props, context){
     const { ripple, rippleOut } = useRipple();
 
-    const onMousedown = (event) => {
+    const onPointerdown = (event) => {
       ripple({event: event, element: event.currentTarget})
     }
-    const onMouseleave = () => {
+    const onPointerleave = () => {
       rippleOut();
     }
     const element = context.slots.default()[0];
     console.log(element)
     console.log(context)
     // return () => h(element.type, {...element.props, onMousedown, onMouseup: onMouseleave, onMouseleave}, element.children)
-    return () => h(element.type, {...context.slots.default()[0].props, onMousedown, onMouseup: onMouseleave, onMouseleave}, context.slots.default()[0].children)
+    return () => h(element.type, {...context.slots.default()[0].props, onPointerdown, onPointerup: onPointerleave, onPointerleave}, context.slots.default()[0].children)
   }
 }
 </script>
