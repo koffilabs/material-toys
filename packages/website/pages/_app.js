934d488b013a9c4d4a88047e48e76a75f13683ca
Nicola Rizzo <nicola.rizzo@gmail.com>
2021-12-26 17:23:01 +0100

    add: Roboto web font from CDN

--- packages/website/pages/_app.js
+++ packages/website/pages/_app.js
@@ -1,3 +1,4 @@
+// TODO: import from node_modules (the library should provide a global css)
 import "../css/main.css";
 export default ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
