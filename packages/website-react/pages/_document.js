d69981a7cc8551ecc6f87e4d1e8952dccebe5777
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-01-21 14:25:47 +0100

    fix: page title changed

--- packages/website-react/pages/_document.js
+++ packages/website-react/pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Material Yue</title>
+          <title>Material Toys</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
