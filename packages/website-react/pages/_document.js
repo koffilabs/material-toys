2cdd1c929ce61f4fed1beeaa0e7f60f1e3a8180e
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-01-28 11:09:43 +0100

    fix: title removed from _document.js

--- packages/website-react/pages/_document.js
+++ packages/website-react/pages/_document.js
@@ -10,7 +10,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <title>Material Toys</title>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link
