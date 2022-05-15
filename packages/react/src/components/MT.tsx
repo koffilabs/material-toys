46c5222191d36e5ae622516771e80847ee730374
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-05-15 16:04:45 +0200

    feat: useTheme changes, added a context for a custom theme function

--- packages/react/src/components/MT.tsx
+++ packages/react/src/components/MT.tsx
@@ -6,15 +6,18 @@ import {
   material_tokens_polyfill,
 } from "@material-toys/common";
 
-const tokens = { ...material_tokens_polyfill, ...material_tokens };
+const defaultTokens = { ...material_tokens_polyfill, ...material_tokens };
 
 interface AppProps {
   children: ReactNode;
+  tokens?: any;
+  theme?: any;
   variant?: string;
 }
-export const MT = ({ children, variant = "" }: AppProps) => {
+export const MT = ({ children, tokens = {}, theme = {}, variant = "" }: AppProps) => {
+  // TODO: add two new contexts for the user provided theme and the tokens
   const { ThemeContext, VariantContext } = useTheme();
-  const [reactiveTokens, setReactiveTokens] = useState(tokens);
+  const [reactiveTokens, setReactiveTokens] = useState(defaultTokens);
   return (
     <ThemeContext.Provider value={reactiveTokens}>
       <VariantContext.Provider value={variant}>
--- packages/react/src/hooks/useTheme.ts
+++ packages/react/src/hooks/useTheme.ts
@@ -1,6 +1,7 @@
 import {createContext} from "react";
 const ThemeContext = createContext({});
 const VariantContext = createContext({});
+const ThemeFunctionContext = createContext(() => {});
 export const useTheme: Function = () => {
-    return {ThemeContext, VariantContext}
+    return {ThemeContext, VariantContext, ThemeFunctionContext}
 }
