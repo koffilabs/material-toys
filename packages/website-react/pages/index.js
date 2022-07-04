0d990256bfe687d37b839cbc6813f1f488570713
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-07-04 21:44:02 +0200

    fix: remove lorem ipsum placeholder

--- packages/website-react/pages/index.js
+++ packages/website-react/pages/index.js
@@ -4,34 +4,7 @@ export default function Index() {
   return (
     <div className={classes}>
       <h1>Material Toys</h1>
-      <h3>A Material You implementation for React - website</h3>
-      <p>this is a button</p>
-      <Card>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam
-          corporis dolorem odio repellat sequi. Ab ad consequatur deserunt dicta
-          dolore eligendi, excepturi natus non, officiis, quam quas tenetur
-          veniam vero.
-        </p>
-      </Card>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum
-        doloremque excepturi expedita laboriosam magnam quibusdam, quisquam quo
-        veniam vero? Aperiam autem dolorum facere impedit iste minima omnis ut.
-        Id.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum
-        doloremque excepturi expedita laboriosam magnam quibusdam, quisquam quo
-        veniam vero? Aperiam autem dolorum facere impedit iste minima omnis ut.
-        Id.
-      </p>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad cum
-        doloremque excepturi expedita laboriosam magnam quibusdam, quisquam quo
-        veniam vero? Aperiam autem dolorum facere impedit iste minima omnis ut.
-        Id.
-      </p>
+      <p>Material Toys is a Material You implementation for React</p>
     </div>
   );
 }
