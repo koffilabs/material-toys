41cd9c3ca77ca479201665961b1e258328c4cfca
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-07-04 21:41:53 +0200

    fix: back to the theme color

--- packages/website-react/pages/index.js
+++ packages/website-react/pages/index.js
@@ -2,7 +2,7 @@ import classes from "./index.module.scss";
 import { Button, Card } from "@material-toys/react";
 export default function Index() {
   return (
-    <div style={{ background: "#ddd" }} className={classes}>
+    <div className={classes}>
       <h1>Material Toys</h1>
       <h3>A Material You implementation for React - website</h3>
       <p>this is a button</p>
