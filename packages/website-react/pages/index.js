1f2a64ebd002a36a3c609269b92202fad3dfe7c9
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-07-11 22:50:05 +0200

    feat: npm / yarn commands in the homepage

--- packages/website-react/pages/index.js
+++ packages/website-react/pages/index.js
@@ -1,10 +1,14 @@
 import classes from "./index.module.scss";
 import { Button, Card } from "@material-toys/react";
+
 export default function Index() {
   return (
     <div>
       <h1>Material Toys</h1>
-      <p>Material Toys is a Material You implementation for React</p>
+      <h4>A Material You implementation for React</h4>
+      <h2>Installation</h2>
+      <code class={classes.code}>npm i material-toys</code>
+      <code class={classes.code}>yarn add material-toys</code>
     </div>
   );
 }
