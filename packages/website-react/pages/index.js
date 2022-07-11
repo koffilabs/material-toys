69ad63d6e87a781b19cec170c4261a9e8d869482
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-07-11 23:05:58 +0200

    fix: installation instructions

--- packages/website-react/pages/index.js
+++ packages/website-react/pages/index.js
@@ -4,11 +4,13 @@ import { Button, Card } from "@material-toys/react";
 export default function Index() {
   return (
     <div>
-      <h1>Material Toys</h1>
-      <h4>A Material You implementation for React</h4>
-      <h2>Installation</h2>
-      <code class={classes.code}>npm i material-toys</code>
-      <code class={classes.code}>yarn add material-toys</code>
+      <h1 className={classes.title}>Material Toys</h1>
+      <div className={classes.subtitle}>
+        A Material You implementation for React
+      </div>
+      <h3 className={classes.title}>Installation</h3>
+      <code className={classes.code}>npm i @material-toys/react</code>
+      <code className={classes.code}>yarn add @material-toys/react</code>
     </div>
   );
 }
