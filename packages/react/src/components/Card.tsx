a878202007f35f024ca825e39b2c35a2b399a232
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-08-27 21:40:45 +0200

    fix: card theme variant

--- packages/react/src/components/Card.tsx
+++ packages/react/src/components/Card.tsx
@@ -5,53 +5,57 @@ import React, {
   useEffect,
   useState,
 } from "react";
-import { css } from "@emotion/css";
-import { applyReactiveStyle, m3 } from "@material-toys/common";
-import { useTheme } from "../hooks/useTheme";
-import { Ripple } from "./Ripple";
+import {css} from "@emotion/css";
+import {applyReactiveStyle, m3} from "@material-toys/common";
+import {useTheme} from "../hooks/useTheme";
+import {Ripple} from "./Ripple";
+import {merge} from "lodash";
+
 interface CardProps {
   disabled: boolean;
   className: string;
   onClick?: Function;
   children?: ReactNode;
 }
-export const Card = ({ children, className }: CardProps) => {
-  const { ThemeContext, VariantContext } = useTheme();
+
+export const Card = ({children, className}: CardProps) => {
+  const {ThemeContext, VariantContext, ThemeFunctionContext} = useTheme();
   const tokens = useContext(ThemeContext);
   const variant: string = useContext(VariantContext);
   const node = useRef(null);
-  const theme = m3(tokens, { variant });
+  const theme = m3(tokens, {variant});
+  const userTheme: any = useContext(ThemeFunctionContext);
+
   let width, height;
   const [cardClass, setCardClass] = useState(
     css(
       applyReactiveStyle({
         target: "components.Card",
-        theme,
+        theme: merge(theme, userTheme(variant)),
       })
     )
   );
   useEffect(() => {
     if (node?.current) {
-      ({ width, height } = (
+      ({width, height} = (
         node.current as HTMLElement
       ).getBoundingClientRect());
-      console.log("new width", width);
       setCardClass(
         css(
           applyReactiveStyle({
             target: "components.Card",
-            theme,
+            theme: merge(theme, userTheme(variant)),
             width,
             height,
           })
         )
       );
     }
-  }, [node]);
+  }, [node, variant]);
   return (
     <Ripple>
       <div ref={node} className={`${cardClass} ${className}`}>
-        <div className="state" />
+        <div className="state"/>
         {children}
       </div>
     </Ripple>
