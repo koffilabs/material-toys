eb302339bd52ad7d5ecdc0128a7b7d2a4df0bfc7
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-08-28 22:27:30 +0200

    feat: logo changes

--- packages/website/pages/components/Logo.jsx
+++ packages/website/pages/components/Logo.jsx
@@ -6,13 +6,14 @@ export const Logo = ({ fill } = { fill: "#474747" }) => {
       height="62.783"
       viewBox="0 0 125.567 62.783"
     >
+      <rect x="0" y="0" width="126" height="62.783" style={{stroke: fill}} strokeWidth={"5"}/>
       <g transform="translate(0 22.5)">
         <path
           d="M64.356,26.856H85.283V-15H64.356Zm-41.856,0H43.428V-15H22.5Z"
           transform="translate(40.283 13.428)"
           fill={fill}
         />
-        <path d="M31.392,13.077,62.783-22.5H0Z" fill={fill} />
+        <path d="M31.392,13.077,62.783-22.5H0Z" fill={"red"} />
         <path
           d="M64.356,26.856H85.283V-15H64.356Zm-41.856,0H43.428V-15H22.5Z"
           transform="translate(40.283 13.428)"
