6d9b1066eb141a09c7755f9c05e51fd27afb1f4f
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-09-27 22:29:18 +0200

    fix: roundedShape helper function name changed

--- packages/common/src/m3/default/filledtext.ts
+++ packages/common/src/m3/default/filledtext.ts
@@ -1,10 +1,10 @@
 import {M3Options} from "./index";
-import {roundShape} from "../../util/shape";
+import {roundedShape} from "../../util/shape";
 import {MdSysShapeCornerExtraSmallTop} from "./polyfill";
 
 export const FilledText = (tokens, options?: M3Options) => {
   const variant = options.variant ?? "";
   return {
-    borderRadius: roundShape({shape: MdSysShapeCornerExtraSmallTop}),
+    borderRadius: roundedShape({shape: MdSysShapeCornerExtraSmallTop}),
   }
 }
