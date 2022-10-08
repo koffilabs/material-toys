8ccb93e40637885eec839bd5745ac1ef2772ca26
Nicola Rizzo <nicola.rizzo@gmail.com>
2022-10-08 22:16:09 +0200

    fix: page name

--- packages/website/pages/text-field.js
+++ packages/website/pages/text-field.js
@@ -29,17 +29,23 @@ export default function text_field_page() {
           </BlockComponentCanvas>
             <CodeBlock code={`import {FilledTextField, MT} from "@material-toys/react";
 import { SearchIcon, OutlinedCancelIcon, OutlinedErrorIcon } from "@material-toys/icons-react";
-<MT>  
-  <FilledTextField supportingText="Supporting text" label={"Label text"}/>
-  <FilledTextField disabled supportingText="Supporting text" label={"Label text"}/>
-  <FilledTextField prefix="$" supportingText="With prefix" label={"Label text"}/>
-  <FilledTextField maxLength={10} characterCounter={true} supportingText="Supporting text" leadingIcon={<SearchIcon />} label={"Label text"}/>
-  <FilledTextField disabled maxLength={10} characterCounter={true} supportingText="Supporting text" leadingIcon={<SearchIcon />} label={"Label text"}/>
-  <FilledTextField onChange={thirdOnChange} value={thirdValue} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedCancelIcon onClick={() => setThirdValue("")} style={{cursor: "pointer"}}/>} label={"Label text"}/>
-  <FilledTextField disabled value={"aaa"} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedCancelIcon onClick={() => setThirdValue("")} style={{cursor: "pointer"}}/>} label={"Label text"}/>
-  <FilledTextField supportingText={"Supporting text"} value={"error"} error={true} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedErrorIcon/>} label={"Label text"}/>
-</MT>  
-              `}></CodeBlock></MT>
+function App() {
+  const [thirdValue, setThirdValue] = useState("abc");
+  const thirdOnChange = (e) => {
+    setThirdValue(e.target.value)
+  }
+  return (
+  <MT>  
+    <FilledTextField supportingText="Supporting text" label={"Label text"}/>
+    <FilledTextField disabled supportingText="Supporting text" label={"Label text"}/>
+    <FilledTextField prefix="$" supportingText="With prefix" label={"Label text"}/>
+    <FilledTextField maxLength={10} characterCounter={true} supportingText="Supporting text" leadingIcon={<SearchIcon />} label={"Label text"}/>
+    <FilledTextField disabled maxLength={10} characterCounter={true} supportingText="Supporting text" leadingIcon={<SearchIcon />} label={"Label text"}/>
+    <FilledTextField onChange={thirdOnChange} value={thirdValue} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedCancelIcon onClick={() => setThirdValue("")} style={{cursor: "pointer"}}/>} label={"Label text"}/>
+    <FilledTextField disabled value={"aaa"} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedCancelIcon onClick={() => setThirdValue("")} style={{cursor: "pointer"}}/>} label={"Label text"}/>
+    <FilledTextField supportingText={"Supporting text"} value={"error"} error={true} leadingIcon={<SearchIcon />} trailingIcon={<OutlinedErrorIcon/>} label={"Label text"}/>
+  </MT>)
+}       `}></CodeBlock></MT>
         </div>
       </main>
     </div>
