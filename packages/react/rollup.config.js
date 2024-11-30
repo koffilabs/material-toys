defea2feb9cd893e7fffc1ac0e3bafbfe492bb65
Nicola Rizzo <nicola.rizzo@gmail.com>
2024-11-30 19:19:16 +0100

    feat: using pnpm instead of npm

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,7 +1,7 @@
 import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 // import typescript from '@rollup/plugin-typescript';
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 import external from "rollup-plugin-peer-deps-external";
 import esbuild from "rollup-plugin-esbuild";
 
