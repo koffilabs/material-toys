5bc3a820cffa18399d3c9542410fef2c97c9085d
Nicola Rizzo <nicola.rizzo@gmail.com>
2024-12-27 08:54:53 +0100

    fix: design tokens

--- packages/common/src/m3/cut/buttons.ts
+++ packages/common/src/m3/cut/buttons.ts
@@ -1,17 +1,15 @@
-import {cutShape} from "../../util/shape";
-import {rgba} from "../../util/rgba";
-import * as tokens from "../default/tokens";
-import * as polyfill  from "../default/polyfill";
-const material_tokens = {...tokens, ...polyfill}
+import { cutShape } from "../../util/shape";
+import { rgba } from "../../util/rgba";
+import { tokens } from "../default/tokens";
+import { tokens_polyfill } from "../default/polyfill";
+const material_tokens = { ...tokens, ...tokens_polyfill };
 interface CutButtonArgs {
   shape: string;
 }
 
-export const cutElevatedButton = ({
-                                    shape
-                                  }: CutButtonArgs) => ({
+export const cutElevatedButton = ({ shape }: CutButtonArgs) => ({
   "&.elevated": {
-    filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, .4)})`,
+    filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, 0.4)})`,
     "& .mt-shape": {
       boxShadow: "none",
       ...cutShape({
@@ -22,50 +20,42 @@ export const cutElevatedButton = ({
       "& .mt-shape": {
         boxShadow: "none",
       },
-      "& .state": {
-      },
-
+      "& .state": {},
     },
     "&:hover:not(:disabled)": {
       "& .mt-shape": {
         boxShadow: "none",
-        "& .state": {
-        },
+        "& .state": {},
       },
     },
-    "& .state": {
-    },
-  }
+    "& .state": {},
+  },
 });
-export const cutFilledButton = ({
-                                    shape
-                                  }: CutButtonArgs) => ({
+export const cutFilledButton = ({ shape }: CutButtonArgs) => ({
   "&.filled": {
     transition: "filter .1s ease-in-out",
-    filter: `drop-shadow(0 0px 0px ${rgba(material_tokens.MdSysColorShadow, .4)})`,
+    filter: `drop-shadow(0 0px 0px ${rgba(material_tokens.MdSysColorShadow, 0.4)})`,
     "& .mt-shape": {
-      ...cutShape({shape}),
+      ...cutShape({ shape }),
     },
 
     "&:hover:not(:disabled)": {
-      filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, .4)})`,
+      filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, 0.4)})`,
       "& .mt-shape": {
         boxShadow: "none",
       },
     },
   },
 });
-export const cutFilledTonalButton = ({
-                                  shape
-                                }: CutButtonArgs) => ({
+export const cutFilledTonalButton = ({ shape }: CutButtonArgs) => ({
   "&.filled-tonal": {
     border: "none",
     backgroundColor: "transparent",
     transition: "filter .1s ease-in-out",
-    filter: `drop-shadow(0 0px 0px ${rgba(material_tokens.MdSysColorShadow, .4)})`,
+    filter: `drop-shadow(0 0px 0px ${rgba(material_tokens.MdSysColorShadow, 0.4)})`,
 
     "&:hover:not(:disabled)": {
-      filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, .4)})`,
+      filter: `drop-shadow(0 2px 1px ${rgba(material_tokens.MdSysColorShadow, 0.4)})`,
       "& .mt-shape": {
         boxShadow: "none",
       },
@@ -76,5 +66,4 @@ export const cutFilledTonalButton = ({
       }),
     },
   },
-
 });
