9ab6e239f091d462720b61f00136c0639931393e
Nicola Rizzo <nicola.rizzo@gmail.com>
2025-01-24 22:26:58 +0100

    chore: unused link removed

--- apps/website/app/page.tsx
+++ apps/website/app/page.tsx
@@ -1,11 +1,9 @@
 "use client";
-import classes from "./index.module.scss";
-import { BlockComponentCanvas } from "../components/BlockComponentCanvas";
-import { CodeBlock } from "../components/CodeBlock";
 import { Button } from "@material-toys/react";
 import Head from "next/head";
-import React from "react";
-import Link from "next/link";
+import { BlockComponentCanvas } from "../components/BlockComponentCanvas";
+import { CodeBlock } from "../components/CodeBlock";
+import classes from "./index.module.scss";
 
 export default function Index() {
   return (
@@ -26,7 +24,6 @@ export default function Index() {
       <h1>Material Toys</h1>
       <p>A Material Design 3 components library for React</p>
       <h2>Getting started</h2>
-      <Link href="/button">Todo: remove me, button page</Link>
       <code className={classes.commands}>npm i @material-toys/react</code>
       <code className={classes.commands}>yarn add @material-toys/react</code>
       <BlockComponentCanvas showGrid={true}>
